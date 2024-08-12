6fbd2d415ab47c52bb43608b7148065a1ba110b2
ManuGrillo752 <mgrillo677@.com>
2024-08-12 16:10:27 -0400

    guardé el archivo

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-let variable = 1;
-let nombre = "Manuel";
+let suma = 2+2
+let resta = 6-2
+let division = 8/2
+let multiplicacion = 4*1 
+
+let nombre = 'Manuel De Andrade'
+
+let entero = 6
+let decimal = 2.5
+let negativo = -6
+let decimalNeg = -2.5
+
+let indefinido 
+
+let nulo = null 
+
+let verdadero = 1>0
+let falso = 1<0
+
+let agrupacion = 2>2 && 3==3
+let opcional = 2<2 || 4>3
+let negacion = !9>8
