445f266296cc9629de399a36ca7cbfb45dfce4d7
ManuGrillo752 <mgrillo677@.com>
2024-08-14 16:06:19 -0400

    Aprendi las condicionales

---
+++ condicionales.js
@@ -0,0 +1,43 @@
+/*let puntuacion = 7
+if (puntuacion > 7) {
+    console.log('Excelente')
+} else if (puntuacion >= 5 && puntuacion <= 7) {
+    console.log('Media')
+} else if(puntuacion>=3 && puntuacion<=4){
+    console.log('Mal')
+}else{
+    console.log('Pesima')
+}
+
+let dia = 0
+switch (dia) {
+    case 1:
+        console.log('Lunes')
+        break;
+    case 2:
+        console.log('Martes')
+        break;
+    case 3:
+        console.log('Miercoles')
+        break;
+    case 4:
+        console.log('Jueves')
+        break;
+    case 5:
+        console.log('Viernes')
+        break;
+    case 6:
+        console.log('Sabado')
+            break;
+    case 7:
+        console.log('Domingo')  
+        break; 
+    default:
+        console.log('Todo bien en casa?')
+        break;
+}
+*/
+
+let edad = 17
+
+edad >= 18 ? console.log('Puede pasar'): console.log('No puede pasar')
