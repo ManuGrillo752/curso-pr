7fd803fd50e035cb9d15165bdec0d1acfbf27fc9
ManuGrillo752 <mgrillo677@.com>
2024-08-26 16:11:15 -0400

    Vimos arreglos

---
+++ arrays.js
@@ -0,0 +1,29 @@
+/*let miNombre = ['Jose', 'Manuel', 'De Andrade', 'Duran']
+console.log(miNombre[0]) */
+
+/*let misDatos = ['Manuel', 174, 18, 'Sushi']
+let ultimoValor = misDatos.pop()
+console.log(ultimoValor)
+console.log(misDatos)*/
+
+/*let misDatos = ['Manuel', 174, 18, 'Sushi']
+misDatos.unshift(21)
+console.log(misDatos)*/
+
+/* misDatos = ['Manuel', 174, 18, 'Sushi']
+misDatos.push(22)
+console.log(misDatos)*/
+
+/*let misDatos = ['Manuel', 174, 18, 'Sushi']
+let primerValor = misDatos.shift()
+console.log(primerValor)
+console.log(misDatos)*/
+
+/*let misDatos = ['Manuel', 174, 18, 'Sushi']
+console.log(misDatos.indexOf(174))*/
+
+/*let misDatos = ['Manuel', 174, 18, 'Sushi', 18]
+console.log(misDatos.indexOf(18))*/
+
+/*let misDatos = ['Manuel', 174, 18, 'Sushi', 18]
+console.log(misDatos.lastIndexOf(18))*/
---
+++ ciclos.js
@@ -0,0 +1,28 @@
+/*for (i = 0;i<=5; i++ ){
+    console.log('Hola')
+
+}*/
+
+/*for(i=1; i<6; i++){
+    console.log(i)
+}*/
+
+/*for(i=1; i<11; i++){
+    console.log('7 x ' + i + ' ' + '=' + ' ' + 7*i) 
+}*/
+/*let acum = 0
+for(i=1; i<11; i++){
+  acum = acum + i
+  console.log(acum)
+}*/
+
+
+let i = 0
+while (i < 5) {
+    i++
+    console.log(i)
+    
+}
+    
+    
+    
